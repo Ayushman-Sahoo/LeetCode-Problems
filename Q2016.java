3451546bd0b7c989b5c469cde7d4d1335a05271b
AYUSHMAN SAHOO <ayushmansahoo648@gmail.com>
2025-06-16 08:51:49 +0530

    Add files via upload

---
+++ Q2016.java
@@ -0,0 +1,58 @@
+package LeetCodes;
+/*Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+Return the maximum difference. If no such i and j exists, return -1.
+Example 1:
+Input: nums = [7,1,5,4]
+Output: 4
+Explanation:
+The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+Example 2:
+Input: nums = [9,4,3,2]
+Output: -1
+Explanation:
+There is no i and j such that i < j and nums[i] < nums[j].
+Example 3:
+Input: nums = [1,5,2,10]
+Output: 9
+Explanation:
+The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.  */
+import java.util.Scanner;
+
+public class Q2016 {
+    public static int maximumDifference(int[] nums) {
+        int min = nums[0];
+        int maxDiff = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > min) {
+                int diff = nums[i] - min;
+                if (diff > maxDiff) {
+                    maxDiff = diff;
+                }
+            } else {
+                min = nums[i];
+            }
+        }
+
+        return maxDiff;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Output result
+        int result = maximumDifference(nums);
+        System.out.println("Maximum Difference: " + result);
+    }
+}
